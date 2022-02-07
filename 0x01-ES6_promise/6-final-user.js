199cc6c932551657b852d2eb84d848b24f661237
Esoteric918 <3411@holbertonschool.com>
2022-02-07 11:55:56 -0600

    refactor for err handle

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      console.log(values);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const result = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    result.push({ status: 'fulfilled', value: user});
+    await uploadPhoto(fileName);
+  } catch (err) {
+    result.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
     });
+  }
+  return result;
 }
