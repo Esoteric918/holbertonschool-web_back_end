975abd3d83b587d20080ca18b2efc2545a9ad265
Esoteric918 <3411@holbertonschool.com>
2022-02-08 10:26:24 -0600

    make class with atts

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+    if (typeof name !== 'string') throw Error('name must be a String');
+    if (typeof length !== 'number') throw Error('length must be a Number');
+    if (!Array.isArray(students)) throw Error('students must be an Array');
+    for(const arr of students) {
+      if (typeof arr !== 'string') throw Error('student must be a Sting');
+    }
+    }
+  set name(name) {
+    if (typeof name !== 'string') throw Error('name must be a String');
+    this._name = name;
+  }
+  get name() {
+    return this._name
+  }
+  set length(length) {
+    if (typeof length !== 'number') throw Error('length must be a Number');
+    this._length = length;
+  }
+  get length() {
+    return this._length;
+  }
+  set students(students) {
+    if (!Array.isArray(students)) throw Error('students must be an Array');
+    for(const arr of students) {
+      if (typeof arr !== 'string') throw Error('student must be a Sting');
+    this._students = students;
+    }
+  }
+  get students() {
+    return this._students;
+  }
+}
