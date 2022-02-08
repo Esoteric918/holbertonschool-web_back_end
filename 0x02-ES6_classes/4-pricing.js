b76b9403f8cbc4235d27ede01d04331483961a6b
Esoteric918 <3411@holbertonschool.com>
2022-02-08 12:32:10 -0600

    lint fix

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,19 +1,22 @@
-import Currency from "./3-currency";
+/* eslint-disable no-unused-vars */
+import Currency from './3-currency';
+
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
+
   get amount() {
-    this._amount
+    return this._amount;
   }
 
   set amount(amount) {
-    this._amount = amount
+    this._amount = amount;
   }
 
   get currency() {
-    this._currency;
+    return this._currency;
   }
 
   set currency(currency) {
@@ -21,9 +24,10 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
-   static convertPrice(amount, conversionRate) {
+
+  static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
-   }
+  }
 }
