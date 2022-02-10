af59d6619a3c9c06c57a17cd2c292a381fb933b3
Esoteric918 <3411@holbertonschool.com>
2022-02-10 09:44:39 -0600

    +func to confrim str

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,19 @@
-
 export default function cleanSet(set, startString) {
-  if (!set || !startString || !(typeof startString === 'string')); {
+  if (!set || !startString || !(typeof startString === 'string')) {
     return '';
-  }
-};
+
+}
+
+function aStr(str) {
+  return str !== '' && (typeof str === 'string')
+}
 
 const newArr = [];
 
-newArr.forEach((str) => {
-  if (newArr(str) && str.startsWith(startString)) {
-    newArr.push(str.splice(startString, ''));
+set.forEach((str) => {
+  if (aStr(str) && str.startsWith(startString)) {
+    newArr.push(str.replace(startString, ''));
   }
-  return newArr.join('-');
 });
-
-
-
+  return newArr.join('-');
+};
