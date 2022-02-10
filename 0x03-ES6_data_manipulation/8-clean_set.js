446247ff17152296f0826e4c58af9a2ea89b1a2b
Esoteric918 <3411@holbertonschool.com>
2022-02-10 09:46:21 -0600

    fix lint issue

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,19 +1,18 @@
 export default function cleanSet(set, startString) {
   if (!set || !startString || !(typeof startString === 'string')) {
     return '';
-
-}
-
-function aStr(str) {
-  return str !== '' && (typeof str === 'string')
-}
-
-const newArr = [];
-
-set.forEach((str) => {
-  if (aStr(str) && str.startsWith(startString)) {
-    newArr.push(str.replace(startString, ''));
   }
-});
+
+  function aStr(str) {
+    return str !== '' && (typeof str === 'string');
+  }
+
+  const newArr = [];
+
+  set.forEach((str) => {
+    if (aStr(str) && str.startsWith(startString)) {
+      newArr.push(str.replace(startString, ''));
+    }
+  });
   return newArr.join('-');
-};
+}
