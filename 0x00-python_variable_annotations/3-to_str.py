6915d941d3b1327a0cb942ebd4dbdec4620356b1
Esoteric918 <3411@holbertonschool.com>
2022-06-01 18:00:40 -0500

    fix bin bash

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-''' type-annotated function to_str that takes a float n as argument and
- returns the string representation of the float'''
+''' type-annotated function'''
 
 def to_str(n: float) -> str:
-    ''' type-annotated function to_str which takes a float n as argument'''
+    '''takes a float n as argument
+    and returns a string representation of n'''
     return str(n)
