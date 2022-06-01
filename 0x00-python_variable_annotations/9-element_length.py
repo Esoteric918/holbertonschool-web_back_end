94836d5edc225e7dc19f5bf817ca2601daf221e1
Esoteric918 <3411@holbertonschool.com>
2022-06-01 18:44:00 -0500

    fix doc str and return annotation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-'''Annotate the below function’s parameters and return values with the appropriate types
+'''Annotate the below function’s parameters
+and return values with the appropriate types
 '''
 from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    '''Annotate the below function’s parameters and return values with the appropriate types'''
+    '''return values with the appropriate types'''
     return [(item, len(item)) for item in lst]
