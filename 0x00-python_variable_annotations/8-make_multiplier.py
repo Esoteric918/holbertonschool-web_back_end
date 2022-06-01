7514bf80918715083ee698ef0a949a938e3c8236
Esoteric918 <3411@holbertonschool.com>
2022-06-01 18:42:00 -0500

    fix doc str and return annotation

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-'''  type-annotated function make_multiplier that takes
-a float multiplier as argument and
-returns a function that multiplies a float by multiplier.'''
+'''  type-annotated function'''
 
 from typing import Callable
+
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     ''' type-annotated function that takes a float multiplier as argument'''
