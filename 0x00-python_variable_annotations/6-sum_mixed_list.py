6f90f77a8f343d2ac16319dc5b59959ea4359d19
Esoteric918 <3411@holbertonschool.com>
2022-06-01 18:06:28 -0500

    pycodestyle fix

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,6 +6,5 @@
 
 
 def sum_mixed_list(mxd_lst):
-    ''' type-annotated function sum_mixed_list which takes a list mxd_lst of integers and floats
-    and returns their sum as a float '''
+    '''returns the sum as a float '''
     return sum(mxd_lst)
