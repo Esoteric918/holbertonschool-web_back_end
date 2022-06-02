c97c8d56cd52a864d00864d7d5cd25484b766719
Esoteric918 <3411@holbertonschool.com>
2022-06-02 09:58:42 -0500

    refactor w/res_list

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,13 +2,18 @@
 '''execute multiple coroutines at the same time with async'''
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+import asyncio
 from typing import List
 
 
 async def wait_n(n, max_delay) -> List[float]:
     '''return the list of all the delays (float values)'''
-    delays = []
+    delays: List = []
     for i in range(n):
-        delay = await wait_random(max_delay)
+        delay = asyncio.create_task(wait_random(max_delay))
         delays.append(delay)
-    return delays
+    res_list: List = []
+    for task in asyncio.as_completed(delays):
+        res: float = await task
+        res_list.append(res)
+    return res_list
