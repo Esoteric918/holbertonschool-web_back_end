8aaee255b33ab4aea46210b0ef70b9f62e92dfed
Esoteric918 <3411@holbertonschool.com>
2022-06-02 09:04:33 -0500

    not working ???

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,7 +6,7 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    '''returns the first element of a list or 0 if the list is empty'''
+    '''returns the first element of a list or None if the list is empty'''
     if lst:
         return lst[0]
     else:
