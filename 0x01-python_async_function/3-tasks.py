644ad89a098145859cf14642016b5aa33a76c2d9
Esoteric918 <3411@holbertonschool.com>
2022-06-03 08:29:32 -0500

    pycodestyle fix

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-
-wait_random = __import__('0-basic_async_syntax').wait_random
+'''async function with annotation'''
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay: int) -> None:
+    '''task that waits for a random number of seconds between 0 and max_delay'''
     task = asyncio.create_task(wait_random(max_delay))
     return task
