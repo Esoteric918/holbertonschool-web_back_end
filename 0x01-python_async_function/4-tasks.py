877001043de2941924db291fbf6fc3544e649089
Esoteric918 <3411@holbertonschool.com>
2022-06-03 08:33:45 -0500

    fix arg in func

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 '''execute multiple coroutines at the same time with async'''
 
-task_wait_random = __import__('3-tasks').task_wait_random
 import asyncio
 from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay) -> List[float]:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     ''' Async function that calls task_wait_random n number of times'''
     delays: List = []
     for i in range(n):
