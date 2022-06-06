7c6638f489292c5140805a7728ca56dad2719aec
Esoteric918 <3411@holbertonschool.com>
2022-06-06 12:40:56 -0500

    refactor w/ no empty list

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     '''async_comprehension coroutine'''
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    return [i async for i in async_generator()]
