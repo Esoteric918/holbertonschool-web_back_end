ce0a146247a40be526fbc813abae1399f9252e85
Esoteric918 <3411@holbertonschool.com>
2022-06-06 10:59:49 -0500

    refactor

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,14 +2,13 @@
 '''coroutine async_generator'''
 
 import asyncio
-from typing import Generator
+from typing import Generator, List
 from random import uniform
 
 
 async def async_generator() -> Generator[float, None, None]:
     '''async_generator'''
-    result = []
     for i in range(10):
-        result.append(uniform(0, 10))
-        yield result[-1]
         await asyncio.sleep(1)
+        yield uniform(0, 10)
+
