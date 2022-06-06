08d9f9a8210bc4d041099ccc8a9b33710aef0710
Esoteric918 <3411@holbertonschool.com>
2022-06-06 08:29:58 -0500

    coroutine async_generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''coroutine async_generator'''
+
+import asyncio
+from typing import Generator
+from random import uniform
+
+
+async def async_generator() -> Generator[float, None, None]:
+    '''async_generator'''
+    result = []
+    for i in range(10):
+        result.append(uniform(0, 10))
+        yield result[-1]
+        await asyncio.sleep(.1)
