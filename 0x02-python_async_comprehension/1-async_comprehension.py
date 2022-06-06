07ffde6e9ff5e67050894d10f33ebf3f37a5c83e
Esoteric918 <3411@holbertonschool.com>
2022-06-06 08:47:37 -0500

    async comperhension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''async_comprehension'''
+
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    '''async_comprehension coroutine'''
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
