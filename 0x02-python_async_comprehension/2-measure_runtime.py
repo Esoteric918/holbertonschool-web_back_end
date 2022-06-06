56f31e4da3558fe0d03c8b9911e59df90bad201e
Esoteric918 <3411@holbertonschool.com>
2022-06-06 09:03:44 -0500

    measure_runtime of coroutine

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''measure_runtime of coroutine'''
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime()-> float:
+    '''measure_runtime coroutine'''
+    start: float = time.time()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    end: float = time.time()
+    return end - start
