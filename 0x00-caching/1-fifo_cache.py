3e779f07e6ab902c9198f2bbde4bc8a258b1f77b
Esoteric918 <3411@holbertonschool.com>
2022-06-09 08:14:36 -0500

    refactor same output

--- 0x00-caching/1-fifo_cache.py
+++ 0x00-caching/1-fifo_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from typing import OrderedDict
 from base_caching import BaseCaching
+from typing import OrderedDict
 
 
 class FIFOCache(BaseCaching):
@@ -12,14 +12,13 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         ''' put item in cache '''
-        if key is not None and item is not None:
+        if key and item:
             self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            key = self.cache_data.popitem(last=False)
-            print('DISCARD:{}'.format(key[0]))
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                key = self.cache_data.popitem(last=False)
+                print('DISCARD:{}'.format(key[0]))
 
     def get(self, key):
         ''' get item from cache '''
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data.get(key)
+        if key or key in self.cache_data:
+            return self.cache_data.get(key)
