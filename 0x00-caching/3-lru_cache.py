f18ae542acaf10a4ab8ab327044d27df732ad2be
Esoteric918 <3411@holbertonschool.com>
2022-06-09 10:06:53 -0500

    lru cache system

---
+++ 0x00-caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''LRU Cache'''
+
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+
+    def __init__(self):
+        ''' initialize LRU cache '''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''add item to cache'''
+        if key and item:
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+            if len(self.cache_data) > self.MAX_ITEMS:
+                key = self.cache_data.popitem(last=False)
+                print('DISCARD: {}'.format(key[0]))
+
+    def get(self, key):
+        ''' get item from cache '''
+        if key and key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data.get(key)
