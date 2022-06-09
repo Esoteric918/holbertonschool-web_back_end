3940ca12153498c7d872245c442f103f158c10ae
Esoteric918 <3411@holbertonschool.com>
2022-06-09 09:36:35 -0500

    revert print statment

--- 0x00-caching/1-fifo_cache.py
+++ 0x00-caching/1-fifo_cache.py
@@ -16,10 +16,10 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                key = self.cache_data.popitem(last=False)
-                print('DISCARD:{}'.format(key[0]))
+                key, val = self.cache_data.popitem(last=False)
+                print('DISCARD:{}'.format(key))
 
     def get(self, key):
         ''' get item from cache '''
-        if key or key not in self.cache_data:
+        if key or key in self.cache_data:
             return self.cache_data.get(key)
