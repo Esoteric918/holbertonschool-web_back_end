e680ec088c678e2331008fb34dff84320629386d
Esoteric918 <3411@holbertonschool.com>
2022-06-09 09:21:03 -0500

    fix line spacing

--- 0x00-caching/2-lifo_cache.py
+++ 0x00-caching/2-lifo_cache.py
@@ -2,7 +2,8 @@
 ''' 2-lifo_cache.py: LIFO Cache '''
 
 from base_caching import BaseCaching
-from typing import OrderedDicts
+
+
 class LIFOCache(BaseCaching):
     '''Inherit from BaseCaching and implement the following methods:'''
 
