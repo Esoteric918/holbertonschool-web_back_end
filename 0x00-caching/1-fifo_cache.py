b310f804b1f2dc5c6f5c857eb227b9b12b1800ea
Esoteric918 <3411@holbertonschool.com>
2022-06-09 07:37:28 -0500

    pycodestyle fix

--- 0x00-caching/1-fifo_cache.py
+++ 0x00-caching/1-fifo_cache.py
@@ -3,11 +3,13 @@
 from typing import OrderedDict
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
     ''' FIFO cache implementation '''
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
+
     def put(self, key, item):
         ''' put item in cache '''
         if key is None or item is None:
@@ -16,6 +18,7 @@ class FIFOCache(BaseCaching):
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             key = self.cache_data.popitem(last=False)
             print('DISCARD:', key[0])
+
     def get(self, key):
         ''' get item from cache '''
         if key is None:
