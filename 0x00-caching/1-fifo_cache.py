8b98a618cc057ed062fe503b9994fbeb1fc1bd47
Esoteric918 <3411@holbertonschool.com>
2022-06-09 08:05:04 -0500

    pycodestyle fix

--- 0x00-caching/1-fifo_cache.py
+++ 0x00-caching/1-fifo_cache.py
@@ -12,15 +12,14 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         ''' put item in cache '''
-        if key is None or item is None:
-            return None
-        self.cache_data[key] = item
+        if key is not None and item is not None:
+            self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             key = self.cache_data.popitem(last=False)
-            print('DISCARD:', key[0])
+            print('DISCARD:{}'.format(key[0]))
 
     def get(self, key):
         ''' get item from cache '''
-        if key is None:
+        if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
