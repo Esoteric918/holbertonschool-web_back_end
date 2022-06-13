7d7eef6d19c5edf39187b958d525b9153f6f2b16
Esoteric918 <3411@holbertonschool.com>
2022-06-13 10:15:14 -0500

    task 0 helper func

---
+++ 0x01-pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''implement a simple helper function to paginate a list'''
+
+from typing import Union, Tuple
+
+
+def index_range(page: int, page_size: int) -> Union[Tuple[int, int], None]:
+    '''return ta tuple of the size of start and end index'''
+    if page == 0 and page_size:
+        start = 0
+        end =page_size
+        return start, end
+    elif page and page_size:
+        start = (page - 1) * page_size
+        end = page * page_size
+        return start, end
+    else:
+        return None
