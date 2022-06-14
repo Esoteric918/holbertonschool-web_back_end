b400333bd5399b4ec2b77fffe191ea13b35df611
Esoteric918 <3411@holbertonschool.com>
2022-06-14 08:24:27 -0500

    pycode fix

--- 0x01-pagination/0-simple_helper_function.py
+++ 0x01-pagination/0-simple_helper_function.py
@@ -8,7 +8,7 @@ def index_range(page: int, page_size: int) -> Union[Tuple[int, int], None]:
     '''return ta tuple of the size of start and end index'''
     if page == 0 and page_size:
         start = 0
-        end =page_size
+        end = page_size
         return start, end
     elif page and page_size:
         start = (page - 1) * page_size
