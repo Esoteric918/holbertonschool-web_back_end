c4c235d3f61db291af127d5ef8c74db7be3bab64
Esoteric918 <3411@holbertonschool.com>
2022-06-15 08:39:41 -0500

    task 0  message obfuscated

---
+++ 0x02-personal_data/filtered_logger.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''Write a function called filter_datum that returns the log message obfuscated:
+
+Arguments:
+fields: a list of strings representing all fields to obfuscate
+redaction: a string representing by what the field will be obfuscated
+message: a string representing the log line
+separator: a string representing by which character is separating all fields in the log line (message)
+The function should use a regex to replace occurrences of certain field values.
+filter_datum should be less than 5 lines long and use re.sub to perform the substitution with a single regex.'''
+
+
+
+import re
+
+
+
+
+def filter_datum(fields, redaction, message, separator):
+    '''
+    :param fields: list of strings representing all fields to obfuscate
+    :param redaction: string representing by what the field will be obfuscated
+    :param message: string representing the log line
+    :param separator: string representing by which character is separating all fields in the log line (message)
+    :return: string representing the log line obfuscated
+    '''
+    for string in fields:
+        regex = fr"(?<={string}=).*?(?={separator})"
+        message = re.sub(r'{}'.format(regex), redaction, message)
+    return message
