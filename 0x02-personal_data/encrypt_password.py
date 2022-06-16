fa6bdaf7adbf4f59ad38ef80b3019fcd2ff06867
Esoteric918 <3411@holbertonschool.com>
2022-06-16 12:55:57 -0500

    task 6 validate pw

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -12,3 +12,14 @@ def hash_password(password: str) -> bytes:
         string representing the hashed password'''
 
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''
+    Args:
+        hashed_password: string representing the hashed password
+        password: string representing the password to be checked
+    Return:
+        True if the password is valid, False otherwise'''
+
+    return bcrypt.checkpw(password.encode(), hashed_password)
