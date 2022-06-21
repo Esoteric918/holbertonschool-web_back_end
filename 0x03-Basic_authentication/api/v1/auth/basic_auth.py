257c3d76f4f2bf8a4867e0b120fbe3f75300de36
Esoteric918 <3411@holbertonschool.com>
2022-06-21 00:51:10 -0500

    task 6 validate pw

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,11 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
+    '''Create a class BasicAuth that inherits from Auth'''
 
+    def __init__(self):
+        '''Initialize the class'''
+        super().__init__()
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         '''Extract the base64 encoded authorization header'''
