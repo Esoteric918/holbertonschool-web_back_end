b1d713fe6b22c8178654b6477b13ace81e66f699
Esoteric918 <3411@holbertonschool.com>
2022-06-23 12:28:44 -0500

    task 6 no output

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -4,10 +4,12 @@
 
 import uuid
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
     '''Class session_auth'''
+
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
@@ -23,3 +25,12 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        '''Get current user id based cookie'''
+
+        session_id = self.session_cookie(request)
+
+        user_id = self.user_id_for_session_id(session_id)
+        print(user_id)
+        return User.get(user_id)
