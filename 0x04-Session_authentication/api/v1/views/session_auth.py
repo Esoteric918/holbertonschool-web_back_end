3e491f19cf4025d0ef46d277618e4112aaf5af14
Esoteric918 <3411@holbertonschool.com>
2022-06-23 15:05:53 -0500

    task 6 and 7

--- 0x04-Session_authentication/api/v1/views/session_auth.py
+++ 0x04-Session_authentication/api/v1/views/session_auth.py
@@ -20,17 +20,16 @@ def login():
     if not pwd:
         return jsonify({"error": "password missing"}), 400
 
-    search = User.search({'email': email})
+    userEmail = User.search({'email': email})
 
-    if not search:
+    if not userEmail or len(userEmail) == 0:
         return jsonify({"error": "no user found for this email"}), 404
 
-    if not search[0].is_valid_password(pwd):
+    if not userEmail[0].is_valid_password(pwd):
         return jsonify({"error": "wrong password"}), 401
-
     from api.v1.app import auth
-    auth.create_session(search[0].id)
-    # set SESSION_ID in cookie to search.id
-    res = jsonify(search[0].to_json())
-    res.set_cookie(getenv('SESSION_NAME'), search[0].id)
+    auth.create_session(userEmail[0].id)
+    # set SESSION_ID in cookie to userEmail.id
+    res = jsonify(userEmail[0].to_json())
+    res.set_cookie(getenv('SESSION_NAME'), userEmail[0].id)
     return res
