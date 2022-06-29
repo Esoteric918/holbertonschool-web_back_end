371c5381c4dd3828accda83490bcc2563029b420
Esoteric918 <3411@holbertonschool.com>
2022-06-28 20:08:28 -0500

    udated ignore and task 10

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -38,6 +38,15 @@ class Auth:
         except Exception:
             return False
 
+    def create_session(self, email: str) -> str:
+        '''Create a new session for a user'''
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
 
 def _hash_password(password: str) -> bytes:
     """Hash a password
