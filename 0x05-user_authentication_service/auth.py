b22a4bdd670cd755e454a396dd477ee29f4a88a0
Esoteric918 <3411@holbertonschool.com>
2022-06-28 22:39:19 -0500

    task 13

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -56,6 +56,14 @@ class Auth:
         except Exception:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        '''Destroy a session'''
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+            self._db.update_user(user.id, session_id=None)
+            return None
+        except Exception:
+            return False
 
 def _hash_password(password: str) -> bytes:
     """Hash a password
