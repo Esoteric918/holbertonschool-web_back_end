25a50220d6a7978ff68fd1c11a517c98b0246ecc
Esoteric918 <3411@holbertonschool.com>
2022-06-28 22:41:30 -0500

    implement Auth.destroy_session

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -58,12 +58,12 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         '''Destroy a session'''
-        try:
+        if user_id:
             user = self._db.find_user_by(user_id=user_id)
             self._db.update_user(user.id, session_id=None)
+        else:
             return None
-        except Exception:
-            return False
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password
