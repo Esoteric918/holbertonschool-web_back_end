21938fcb188055ee78b7375991a34b7909dd7c15
Esoteric918 <3411@holbertonschool.com>
2022-06-29 20:42:58 -0500

    fix abort status

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -81,7 +81,8 @@ def get_reset_password_token():
     try:
         email = request.form.get('email')
         token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": email, "reset_token": token}), 200
+        return jsonify({"email": email, "reset_token": token})
+
     except Exception:
         abort(403)
 
