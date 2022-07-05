da2400cacf1c746d7fde75471f5170ab6740fbca
Esoteric918 <3411@holbertonschool.com>
2022-07-05 15:08:45 -0500

    task 5 test GithubOrgClient._public_repos_url

--- 0x00-Unittests_and_integration_tests/test_client.py
+++ 0x00-Unittests_and_integration_tests/test_client.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+'''build out tests for client.py'''
 
 from client import GithubOrgClient
 import unittest
 from unittest import mock
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import PropertyMock, patch
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -15,8 +16,16 @@ class TestGithubOrgClient(unittest.TestCase):
         ('abc'),
     ])
 
-    @patch('client.get_json',mock.Mock(return_value={'key':'value'}))
+    @patch('client.get_json', mock.Mock(return_value={'key':'value'}))
     def test_org(self, org):
         '''test if valid org is returned'''
         cls = GithubOrgClient(org)
         self.assertEqual(cls.org, {'key':'value'})
+
+    def test_public_repos_url(self):
+        '''test GithubOrgClient._public_repos_url method'''
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock,
+                   return_value={'repos_url': 'url'}):
+            mockProp = GithubOrgClient('org_prop')
+            self.assertEqual(mockProp._public_repos_url, 'url')
