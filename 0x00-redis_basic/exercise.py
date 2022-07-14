4722fbf423c30f177bf9e2c52072d30d03b32a6a
Esoteric918 <3411@holbertonschool.com>
2022-07-14 15:16:34 -0500

    task 2

--- 0x00-redis_basic/exercise.py
+++ 0x00-redis_basic/exercise.py
@@ -2,16 +2,32 @@
 ''' Exercise: Redis basic '''
 
 from re import S
-from typing import Union
+from typing import Callable, Union
 import uuid
 import redis
 
+
+def count_calls(method: Callable) -> Callable:
+    '''counts the number of time Cache is called'''
+
+    key = method.__qualname__
+
+    def wrapper(self, *args, **kwargs) -> Union[str, int]:
+        '''wrapper to increment the number of calls'''
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
 class Cache:
     ''''A Redis-based cache'''
+
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         ''' Store data in the cache '''
         key = str(uuid.uuid4())
