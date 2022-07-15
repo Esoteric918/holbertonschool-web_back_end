ffd07bba69cffc3aa194d04001e60dd96170d413
Esoteric918 <3411@holbertonschool.com>
2022-07-15 14:01:08 -0500

    task 1 and 2 from sandbox

---
+++ 0x01-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- set new user table
+-- with new attribute country enum('US', 'UK', 'CA')
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'UK', 'CA')
+);
