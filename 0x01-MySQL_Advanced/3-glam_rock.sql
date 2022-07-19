23352f5cba18d067762fc84c3dee78f3371b1b92
Esoteric918 <3411@holbertonschool.com>
2022-07-18 22:00:36 -0500

    task 3

--- 0x01-MySQL_Advanced/3-glam_rock.sql
+++ 0x01-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,6 @@
 -- Column names must be: band_name and lifespan (in years)
 -- You should use attributes formed and split for computing the lifespan
--- Your script can be executed on any database
--- You can use any database
+
 
 SELECT band_name, IFNull(split, 2022) - formed AS lifespan
 FROM metal_bands
