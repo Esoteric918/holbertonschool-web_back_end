d0a61c70649cd890a27a861908796736dc2b4f92
Esoteric918 <3411@holbertonschool.com>
2022-07-19 10:02:22 -0500

    task 11

--- 0x01-MySQL_Advanced/11-need_meeting.sql
+++ 0x01-MySQL_Advanced/11-need_meeting.sql
@@ -2,4 +2,8 @@
 
 CREATE VIEW need_meeting AS
 SELECT name FROM students
-WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 30 DAY));
+WHERE
+    score < 80 AND
+    (last_meeting IS NULL
+        OR
+    last_meeting < ADDDATE(CURDATE(), INTERVAL -30 DAY));
