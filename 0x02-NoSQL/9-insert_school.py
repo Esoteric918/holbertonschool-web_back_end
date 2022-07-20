47cb86c31c33c1faaee001a4c57a7a89b3cb4e6e
Esoteric918 <3411@holbertonschool.com>
2022-07-20 13:43:50 -0500

    task 9

--- 0x02-NoSQL/9-insert_school.py
+++ 0x02-NoSQL/9-insert_school.py
@@ -5,7 +5,5 @@ from pymongo import MongoClient
 
 
 def insert_school(mongo_collection, **kwargs):
-    ''' insert a school document '''
-    if mongo_collection is None:
-        return []
-    return mongo_collection.insert_one(kwargs)
+    '''insert_school'''
+    return mongo_collection.insert_one(kwargs).inserted_id
