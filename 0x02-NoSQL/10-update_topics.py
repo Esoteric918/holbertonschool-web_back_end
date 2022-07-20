ef1b7f81fb70aab49a8cf30a794ba7e3fd117678
Esoteric918 <3411@holbertonschool.com>
2022-07-20 13:58:13 -0500

    task 10

--- 0x02-NoSQL/10-update_topics.py
+++ 0x02-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
     '''Python function to change the topics of a school'''
-    return mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}}).modified_count
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}}).modified_count
