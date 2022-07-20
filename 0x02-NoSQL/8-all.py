24ad9a0211c8d9fbedeab0ba2202b40ed1605a4a
Esoteric918 <3411@holbertonschool.com>
2022-07-20 14:07:22 -0500

    task 8

--- 0x02-NoSQL/8-all.py
+++ 0x02-NoSQL/8-all.py
@@ -5,6 +5,4 @@ from pymongo import MongoClient
 
 def list_all(mongo_collection):
     ''' list all documents in a collection '''
-    if mongo_collection is None:
-        return []
     return mongo_collection.find()
