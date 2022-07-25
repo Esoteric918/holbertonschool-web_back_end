0a30169165fc1961cf526e8aacafe95876c83024
Esoteric918 <3411@holbertonschool.com>
2022-07-25 12:47:21 -0500

    lint fix

--- 0x00-Node_JS_basic/4-http.js
+++ 0x00-Node_JS_basic/4-http.js
@@ -1,12 +1,13 @@
 // create a small HTTP server using the http module:
 
-const http = require("http");
-const host = "localhost";
+const http = require('http');
+
+const host = 'localhost';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello Holberton School!");
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 }).listen(port, host);
 
 module.exports = app;
