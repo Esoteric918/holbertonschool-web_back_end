bcc37552bcd2aecf449964d7853ba2d937acb75b
Esoteric918 <3411@holbertonschool.com>
2022-07-25 12:30:07 -0500

    refactor for correct output

--- 0x00-Node_JS_basic/3-read_file_async.js
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -1,36 +1,39 @@
 const fs = require('fs');
-const { promisify } = require('util');
 
 const countStudents = async (path) => {
-  const data = ''
+  let data = '';
 
   try {
-    data = await fs.promises.readFile(path, 'utf8');
+    data = await fs.promises.readFile(path, 'utf-8');
   } catch (error) {
     throw new Error('Cannot load the database');
   }
 
-  const dataSet = data.split('\n').splice(1); // remove the header
-  const dataRes = dataSet.map((line) => line.split(','))
-    .filter((student) => student.length === 3); // remove the invalid lines for the database
+  let lines = data.split('\n');
+  lines = lines.filter(line => line !== '').slice(1);
+  console.log(`Number of students: ${lines.length}`);
 
-  const students = dataRes.map((student) => ({
-    name: student[0],
-    age: student[1],
-    grade: student[2],
-    field: student[3],
-  })); // create an array of students
+  const fields = lines.map(line => line.split(',')[3]);
+  const unique = [...new Set(fields)];
 
-  const csStudents = students.filter((student) => student.field === 'CS')
-    .map((student) => student.firstname);
-  const sweStudents = students.filter((student) => student.field === 'SWE')
-    .map((student) => student.firstname);
+  const newDic = {};
 
-  console.log(`Numbert of students: ${students.length}`);
-  console.log(`Numbert of students in CS: ${csStudents.length}`);
-  console.log(`Numbert of students in SWE: ${sweStudents.length}`);
+  for (let i = 0; i < unique.length; i++) {
+    const count = fields.filter(field => field === unique[i]).length;
 
-  return { students, csStudents, sweStudents };
+    const names = lines.filter(line => line.split(',')[3] === unique[i]);
+
+    console.log(    `Number of students in ${
+      unique[i]
+    }: ${count}. List: ${names.join(', ')}`,
+  );
+
+    newDic[unique[i]] = {
+      count,
+      names,
+    };
+  }
+  return newDic;
 };
 
 module.exports = countStudents;
