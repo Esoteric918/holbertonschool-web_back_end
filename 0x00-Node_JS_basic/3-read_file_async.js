c2b377ad55615480eec6c9f8c0728c59d9ac86ce
Esoteric918 <3411@holbertonschool.com>
2022-07-25 11:53:50 -0500

    refactor for correct output

--- 0x00-Node_JS_basic/3-read_file_async.js
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -1,27 +1,36 @@
 const fs = require('fs');
 const { promisify } = require('util');
 
-const countStudents = promisify(fs.readFile);
-countStudents('./database.csv', 'utf8')
-  .then((data) => {
-    const lines = data.split('\n').slice(1);
-    const fields = lines.map((line) => line.split(','))
-      .filter((student) => student.length === 4);
-    const students = fields.map((student) => ({
-      firstname: student[0],
-      lastname: student[1],
-      age: student[2],
-      field: student[3],
-    }));
-    const csStudents = students.filter((s) => s.field === 'CS')
-      .map((s) => s.firstname);
-    const sweStudents = students.filter((s) => s.field === 'SWE')
-      .map((s) => s.firstname);
-    console.log(`Number of students: ${students.length}`);
-    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-  }).catch(() => {
-    console.log('Cannot load the database');
-  });
+const countStudents = async (path) => {
+  const data = await promisify(fs.readFile)(path, 'utf8');
+
+  try {
+    data = await fs.promises.readFile(path, 'utf8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+
+  const dataSet = data.split('\n').splice(1); // remove the header
+  const dataRes = dataSet.map((line) => line.split(','))
+    .filter((student) => student.length === 3); // remove the invalid lines for the database
+
+  const students = dataRes.map((student) => ({
+    name: student[0],
+    age: student[1],
+    grade: student[2],
+    field: student[3],
+  })); // create an array of students
+
+  const csStudents = students.filter((student) => student.field === 'CS')
+    .map((student) => student.firstname);
+  const sweStudents = students.filter((student) => student.field === 'SWE')
+    .map((student) => student.firstname);
+
+  console.log (`Numbert of students: ${students.length}`);
+  console.log(`Numbert of students in CS: ${csStudents.length}`);
+  console.log(`Numbert of students in SWE: ${sweStudents.length}`);
+
+  return {students, csStudents, sweStudents};
+};
 
 module.exports = countStudents;
