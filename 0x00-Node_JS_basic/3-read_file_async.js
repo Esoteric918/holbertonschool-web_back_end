383f183f05c88285faf039746d7790a56ad70424
Esoteric918 <3411@holbertonschool.com>
2022-07-25 11:06:38 -0500

    lint fix

--- 0x00-Node_JS_basic/3-read_file_async.js
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -1,20 +1,11 @@
-// Create a function named countStudents. It should accept a path in argument (same as in 2-read_file.js)
-// The function should return a Promise
-// If the database is not available, it should throw an error with the text Cannot load the database
-// If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS
-// It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
-// CSV file can contain empty lines (at the end) - and they are not a valid student!
-// The function should return a Promise
-
-const { count } = require('console');
 const fs = require('fs');
 const { promisify } = require('util');
 
 const countStudents = promisify(fs.readFile);
 countStudents('./database.csv', 'utf8')
   .then((data) => {
-    let lines = data.split('\n').slice(1);
-    const fields = lines.map(line => line.split(','))
+    const lines = data.split('\n').slice(1);
+    const fields = lines.map((line) => line.split(','))
       .filter((student) => student.length === 4);
     const students = fields.map((student) => ({
       firstname: student[0],
@@ -29,9 +20,8 @@ countStudents('./database.csv', 'utf8')
     console.log(`Number of students: ${students.length}`);
     console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
     console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-  }
-  ).catch((err) => {
-    console.log('Cannot load the database');
+  }).catch((err) => {
+    console.log('Cannot load the database', err);
   });
 
 module.exports = countStudents;
