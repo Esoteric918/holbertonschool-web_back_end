091c7487623fd403cb0413695176842ddcae5337
Esoteric918 <3411@holbertonschool.com>
2022-07-27 10:46:27 -0500

    fix output

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -20,7 +20,7 @@ const app = http.createServer(async (req, res) => {
         res.write(`Number of students: ${returnValue.total}\n`);
         for (const key in students) {
           if (students.hasOwnProperty(key)) {
-            res.write(`Number of students in ${key}: ${students[key].count}. ${students[key].namesList.join(', ')}`);
+            res.write(`Number of students in ${key}: ${students[key].count}. List: ${students[key].namesList.join(', ')}`);
             if (key !== Object.keys(students)[Object.keys(students).length - 1]) {
               res.write('\n');
             }
