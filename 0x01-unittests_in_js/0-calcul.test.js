68a13b37c3da60f49b9ec128024a889ae862c10c
Esoteric918 <3411@holbertonschool.com>
2022-07-28 11:07:46 -0500

    task 0 test func + readme

---
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const calculateNumber = require('./0-calcul');
+const assert = require('assert');
+
+describe("calculateNumber", () => {
+    it("checks if int is positive", () => {
+      assert.equal(calculateNumber(1, 3), 4);
+      assert.equal(calculateNumber(1, 3.7), 5);
+      assert.equal(calculateNumber(1.2, 3.7), 5);
+      assert.equal(calculateNumber(1.5, 3.7), 6);
+    })
+
+    it("checks if int are negative", () => {
+      assert.equal(calculateNumber(1, -3.4), -2);
+      assert.equal(calculateNumber(-5, -3.5), -8);
+    });
+
+    it("check argument/TypeError", () => {
+        assert.throws(() => calculateNumber(NaN, 0), {name: 'TypeError'});
+    });
+});
