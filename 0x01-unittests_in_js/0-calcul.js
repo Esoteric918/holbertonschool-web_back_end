2a03d914b8f37d7eaf8844f716ae1ee8e0624292
Esoteric918 <3411@holbertonschool.com>
2022-08-01 10:47:44 -0500

    test code commit

--- 0x01-unittests_in_js/0-calcul.js
+++ 0x01-unittests_in_js/0-calcul.js
@@ -4,6 +4,6 @@
 const calculateNumber = (a, b) => {
   if (isNaN(a) || isNaN(b)) throw new TypeError("Argument is not a number");
   return Math.round(a) + Math.round(b);
-}
+};
 
 module.exports = calculateNumber;
