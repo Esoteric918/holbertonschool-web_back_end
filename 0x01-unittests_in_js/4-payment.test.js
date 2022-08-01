38bef35c4489e334247117807d703e58efb397a7
Esoteric918 <3411@holbertonschool.com>
2022-08-01 13:02:29 -0500

    task 4 use stubs

---
+++ 0x01-unittests_in_js/4-payment.test.js
@@ -0,0 +1,15 @@
+const sendPaymentRequestToApi = require('./3-payment.js');
+const Utils = require('./utils.js');
+const sinon = require('sinon');
+const expect = require('chai').expect;
+
+describe('sendPaymentRequestToApi', () => {
+  it('Tests sendpaymentRequestToApi function', () => {
+    const spyCalculateNumber = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const spy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledWith('The total is: 10')).to.be.true;
+    spyCalculateNumber.restore()
+    spy.restore();
+  });
+});
