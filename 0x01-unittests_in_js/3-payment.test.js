10407d3f2c27c429f069fa6105ba2a5ef50f81ca
Esoteric918 <3411@holbertonschool.com>
2022-08-01 12:50:28 -0500

    task 3

--- 0x01-unittests_in_js/3-payment.test.js
+++ 0x01-unittests_in_js/3-payment.test.js
@@ -1,11 +1,11 @@
 const sendPaymentRequestToApi = require('./3-payment.js');
 const Utils = require('./utils.js');
-const spy = require('sinon').spy;
+const sinon = require('sinon');
 const expect = require('chai').expect;
 
 describe('sendPaymentRequestToApi', () => {
   it('Tests sendpaymentRequestToApi function', () => {
-    const spyCalculateNumber = spy(Utils, 'calculateNumber');
+    const spyCalculateNumber = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
     expect(spyCalculateNumber.calledOnce).to.be.true;
     expect(spyCalculateNumber.calledWith('SUM', 100, 20)).to.be.true;
