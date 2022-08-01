61cd024c5ef90b4d375cbb9b1431f8d46e7bf9a5
Esoteric918 <3411@holbertonschool.com>
2022-08-01 10:11:38 -0500

    test code commit

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,25 @@ const assert = require('assert');
 
 describe ('Test suite', () => {
   // Tests for calculateNumber function using Node.js assert
-  it('Test that calculateNumber sums two rounded numbers', () => {
-    assert.equal(calculateNumber(1, 2), 3);
-    assert.equal(calculateNumber(1.1, 2.1), 3);
-    assert.equal(calculateNumber(1.4, 2.4), 3);
-    assert.equal(calculateNumber(1.9, 2.9), 5);
-    assert.equal(calculateNumber(1.5, .5), 3);
-    assert.equal(calculateNumber(2, 0), 2);
-    assert.equal(calculateNumber(-7, 2.1), -5);
-    assert.equal(calculateNumber(8.4, -3), 5);
-    assert.equal(calculateNumber(-10.9, -3.6), -15);
+  it('it round the first argument', () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
   });
-})
+
+  it('it round the second argument', () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
+  });
+
+  it('it should return the right number', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
+  });
+
+  });
