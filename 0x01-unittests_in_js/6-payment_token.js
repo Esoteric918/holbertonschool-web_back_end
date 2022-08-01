a1f6ec1cb637ff8e3370270bcc4d2a27b501a95f
Esoteric918 <3411@holbertonschool.com>
2022-08-01 14:10:04 -0500

    task 6

---
+++ 0x01-unittests_in_js/6-payment_token.js
@@ -0,0 +1,10 @@
+// The function will take an argument called success (boolean)
+// When success is true, it should return a resolved promise with the object {data: 'Successful response from the API' }
+// Otherwise, the function is doing nothing.
+const getPaymentTokenFromAPI = async (success) => {
+  if (success) {
+    return { data: 'Successful response from the API' };
+  }
+}
+
+module.exports = getPaymentTokenFromAPI;
